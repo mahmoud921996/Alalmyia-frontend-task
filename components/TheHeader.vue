5cc05a92587c78b3071673964d489bddcc4a200a
mahmoud sabiha <mahmoudsabiha51@gmail.com>
2022-10-18 13:33:32 +0000

    add prj files

---
+++ components/TheHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <header class="header">
+    <div class="header-container container">
+      <NuxtLink to="/" class="header-logo">
+        <Logo />
+      </NuxtLink>
+      <nav>
+        <ul class="d-flex justify-content-center align-items-center">
+          <li class="fs-md me-5">
+            <NuxtLink to="/"> Home </NuxtLink>
+          </li>
+          <li class="fs-md me-5">
+            <NuxtLink to="/"> About Us </NuxtLink>
+          </li>
+          <li class="fs-md me-5">
+            <NuxtLink to="/shop"> shop </NuxtLink>
+          </li>
+          <li class="fs-md me-5">
+            <NuxtLink to="/"> Contact Us </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <div class="icons d-flex">
+        <NuxtLink to="/signup" class="header-login me-5">Sign Up</NuxtLink>
+        <ul class="d-flex align-items-center">
+          <li class="me-3 pointer">
+            <Icon name="bi:search" />
+          </li>
+          <li class="me-3 pointer">
+            <Icon name="bi:cart" />
+          </li>
+          <li class="me-3 pointer">
+            <Icon name="bi:heart" />
+          </li>
+          <li class="me-3 pointer">
+            <Icon name="bi:person" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: $zindex-fixed;
+
+  &-container {
+    height: $header-height;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    transition: height 0.3s ease;
+    @include media-breakpoint-down(lg) {
+      height: $header-height-md;
+    }
+  }
+  &-logo {
+    width: rem(140px);
+    display: block;
+    @include media-breakpoint-down(lg) {
+      width: rem(80px);
+    }
+  }
+  &-login {
+    background-color: #f8f9fc;
+    padding: 10px;
+    border-radius: 34px;
+  }
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    css: ["@/assets/scss/index.scss"],
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: `@use "@/assets/scss/global.scss" as *;`,
+          },
+        },
+      },
+    },
+    modules: ['nuxt-icon'],
+    buildModules: ["@nuxtjs/google-fonts"],
+    googleFonts: {
+      families: {
+        "Montserrat": [300,400, 500,600, 700],
+      },
+    },
+})
